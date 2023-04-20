01172fa59298621886a21c7978fb8ac13ca01089
Le Nhat Minh <90740750+nhatminh297@users.noreply.github.com>
2023-04-20 23:15:04 +0700

    Update crawlerFB.py

--- crawlerFB.py
+++ crawlerFB.py
@@ -47,12 +47,16 @@ def get_elems(driver, numerofposts = 5):
     return elems[0:numerofposts]
 
 def post_info(dataft): 
-    pageid = str(dataft['content_owner_id_new'])
-    actorid = dataft["page_insights"][pageid]['targets'][0]['actor_id']
-    postid = dataft["page_insights"][pageid]['targets'][0]['post_id']
-    # publish_time = dataft["page_insights"][pageid]['post_context']['publish_time']
-    dictionary = {"post_id" : f"{postid}", "page_id": f"{pageid}", "actor_id":f"{actorid}"}
-    return dictionary
+    try:
+        pageid = str(dataft['content_owner_id_new'])
+        actorid = dataft["page_insights"][pageid]['targets'][0]['actor_id']
+        postid = dataft["page_insights"][pageid]['targets'][0]['post_id']
+        # publish_time = dataft["page_insights"][pageid]['post_context']['publish_time']
+        dictionary = {"post_id" : f"{postid}", "page_id": f"{pageid}", "actor_id":f"{actorid}"}
+        return dictionary
+    except:
+        dictionary = {"post_id" : f"{postid}", "page_id": None, "actor_id": None}
+        return dictionary
 
 def get_posts_info(elems):
     post_data = []
@@ -113,10 +117,10 @@ def crawl_cmts_reps(driver,cursor, post_id):
 
 BASEURL = "https://www.facebook.com/"
 
-username ="cs232khcl@gmail.com"
-password = "definitelynotapassword"
-desktop_user = "DESKTOP-NHATMIN\DELL"
-server = 'DESKTOP-NHATMIN\SQLEXPRESS02'
+username ="yourusername"
+password = "yourpassword"
+desktop_user = "DESKTOP-NHATMIN\DELL"#get by: Win+R => msinfo32 => User Name
+server = 'DESKTOP-NHATMIN\SQLEXPRESS02'# your MS SQL server name
 database = 'FB'
 frequently = 60*5
 
